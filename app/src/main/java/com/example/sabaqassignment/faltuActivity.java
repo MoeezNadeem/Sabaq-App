003ce7d36e14237e377407aba6a74282003be2ee
Moeez Nadeem <bcsf19a039@pucit.edu.pk>
2023-02-14 19:05:28 +0500

    first commit

---
+++ app/src/main/java/com/example/sabaqassignment/faltuActivity.java
@@ -0,0 +1,14 @@
+package com.example.sabaqassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class faltuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faltu);
+    }
+}
